ac10c320341e2007dc091a8f6b58454c87c3f2fd
ZulfiQar Ali <101404958+TECH-ZF@users.noreply.github.com>
2025-02-08 18:13:01 +0500

    Update zf.py

--- zf.py
+++ zf.py
@@ -1,14 +1,2 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-os.system('git pull')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from ZF import ssbbuy
-    ssbbuy()
-elif bit == '32bit':
-    from ZF import ssbbuy
-    ssbbuy()
+from ZF import superuser
+superuser()
